c6278c98e4dfd4afa83b8cc65f9f1886fecc0cac
Makhsin <pmnknb@gmail.com>
2021-01-16 23:35:40 +0700

    homework

--- Ex-02.js
+++ Ex-02.js
@@ -1,4 +1,37 @@
-const qs = "21+1";
+const qs = "21+5-2*9";
 
-const chars = qs.split(' ');
-console.log(parseInt(qs));
+function equation(qs){
+   let chars = qs.split('+').toString();
+   chars = chars.split('-').toString();
+   chars = chars.split('*').toString();
+   chars = chars.split(',');
+   let m = [];
+   let num;
+   for(let i=0 ; qs.length != i ; i++){
+       if (qs[i] == "+"){
+           m.push(qs[i]);
+        }
+        else if (qs[i] == "-"){
+            m.push(qs[i]);
+         }
+        else if (qs[i] == "*"){
+            m.push(qs[i]);
+         }
+   }
+   for(let i=0 ; chars.length != i ; ++i){
+       if (m[i] == "+"){ 
+        chars[0] = parseInt(chars[0])+parseInt(chars[i+1]);
+        num = chars[0];
+       }
+       else if (m[i] == "-"){
+        chars[0] = parseInt(chars[0])-parseInt(chars[i+1]);
+        num = chars[0];
+       }
+       else if (m[i] == "*"){
+        chars[0] = parseInt(chars[0])*parseInt(chars[i+1]);
+        num = chars[0];
+       }
+   }
+   return(num);
+    }
+ console.log(equation(qs));
